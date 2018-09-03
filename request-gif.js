318d0eccb53a09d9b7109486e91a01a36e6d5276
Sean Lorman <sean.lorman@gmail.com>
2018-09-03 13:23:47 -0600

    Validation is working. Make it look peeerrrrttttyyyyy

--- request-gif.js
+++ request-gif.js
@@ -8,46 +8,56 @@ var ajaxson5 = new Vue({
             loading: false,
             imgSrc: null,
             searchQuery: null,
+            riddle: null,
+        
         };
     },
     methods: {
         fetchGif: function(searchQuery) {
-            // get the user's input text from the DOM
+            
             var searchQuery = document.getElementById("userInput");
-             // TODO should be e.g. "dance"
+             
             var value = searchQuery.value;
             // configure a few parameters to attach to our request
             var api_key = "dc6zaTOxFJmzC";
             var tag = "jackson+5+" + value; // TODO should be e.g. "jackson 5 dance"
             this.loading = true;
-            // TODO what do we want this URL to be?
-            fetch(`https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=${tag}`)
-                .then(response => response.ok ? response.json() : Promise.reject(response))
-                .then(results => {
-                    // if the response comes back successfully, the code in here will execute.
-                    
-                    console.log("we received a response!");
-                    console.log(results);
-    
-                    this.imgSrc = results.data.image_url
-                    this.loading = false;
-                    // 1. set the imgSrc value in our data to the GIF's image_url inside results
-                    // 2. clear the error message and loading state (since our request just succeede)
-                })
-                .catch(err => {
-                    // if something went wrong, the code in here will execute instead of the success function
-
-                    
-                    this.errorMessage = 'Sorry, could not load GIF. Try again!';
-                    
-                });
-            // TODO We've just made a request, so this is a good time to
-            // set "loading = true"
+           
+            var getRiddle = document.getElementById("riddleQ");
+            var riddleValue = getRiddle.value;
             
+            if (riddleValue != 5) {
+                this.loading = false;
+                return this.riddle = true;
+               
+            } else {
+                fetch(`https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=${tag}`)
+                    .then(response => response.ok ? response.json() : Promise.reject(response))
+                    .then(results => {
+                        // if the response comes back successfully, the code in here will execute.
+                        
+                        console.log("we received a response!");
+                        console.log(results);
+        
+                        this.imgSrc = results.data.image_url
+                        this.loading = false;
+                        this.riddle = false;
+                        
+                        
+                    })
+                    
+                    .catch(err => {
+                        // if something went wrong, the code in here will execute instead of the success function
 
-                
-            
-
-        },
+                        
+                        this.errorMessage = 'Sorry, could not load GIF. Try again!';
+                        
+                        
+                    }); 
+                }
+        
+        },  
+        
     },
+    
 });
